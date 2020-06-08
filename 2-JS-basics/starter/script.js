1df7c4e09559c309aeed7c007c8e2c59e6149c43
Julio César Hernández Cruz <julio.hernandez@globant.com>
2020-06-07 23:04:13 -0500

    challenge 3

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -57,3 +57,40 @@ if (johnsTeamAvg > mikesTeamAvg && johnsTeamAvg > marysTeamAvg) {
 } else {
     console.log("Draw: " + johnsTeamAvg);
 }
+
+
+
+/*****************************
+* CODING CHALLENGE 3
+*/
+
+/*
+John and his family went on a holiday and went to 3 different restaurants. The bills were $124, $48 and $268.
+
+To tip the waiter a fair amount, John created a simple tip calculator (as a function). He likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+
+In the end, John would like to have 2 arrays:
+1) Containing all three tips (one for each bill)
+2) Containing all three final paid amounts (bill + tip).
+
+(NOTE: To calculate 20% of a value, simply multiply it with 20/100 = 0.2)
+
+GOOD LUCK 😀
+*/
+
+var calculateTip = function(bill) {
+    if (bill > 200) {
+        return bill * 0.1;
+    } else if (bill >= 50 && bill <= 200) {
+        return bill * 0.15;
+    } else {
+        return bill * 0.2;
+    }
+}
+
+var bills = [124, 48, 268];
+var totals = [];
+for (var i = 0; i < bills.length; i++) {
+    totals[i] = bills[i] + calculateTip(bills[i])
+}
+console.log("Bills: " + bills + ", Totals: " + totals)
